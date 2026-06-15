b5a2474426a951ce75ec0fb568decdc3161624ab
kevin-joseph05 <kevin.joseph542@gmail.com>
2026-06-15 18:22:22 -0400

    Added classes for further data cleaning

---
+++ src/pipeline/clean_logs.py
@@ -0,0 +1,36 @@
+from pyspark.sql.functions import col, when, regexp_replace, to_timestamp
+
+class CleanLogs:
+    def __init__(self, df):
+        self.df = df
+
+    def drop_failed_parse(self):
+        self.df = self.df.filter(
+                (col("host") != "") &
+                (col("timestamp") != "") &
+                (col("method") != "") &
+                (col("endpoint") != "") &
+                (col("status") != "") 
+                )
+
+    def normalize_bytes(self):
+        self.df = self.df.withColumn(
+                "bytes",
+                when(col("bytes") == "-", 0).otherwise(col("bytes").cast("int"))
+                )
+
+    def strip_query(self):
+        self.df = self.df.withColumn("endpoint", regexp_replace("endpoint", r"\?.*$", ""))
+
+    def cast_timestamp(self):
+        self.df = self.df.withColumn("timestamp", to_timestamp("timestamp", "dd/MMM/yyyy:HH:mm:ss Z"))
+
+    def clean_parsed_df(self):
+        self.drop_failed_parse()
+        self.normalize_bytes()
+        self.strip_query()
+        self.cast_timestamp()
+        return self.df
+
+if __name__ == "__main__":
+    pass
--- src/pipeline/parse_logs.py
+++ src/pipeline/parse_logs.py
@@ -1,19 +1,22 @@
-from pyspark.sql.functions import regexp_extract
+from pyspark.sql.functions import regexp_extract, col
 
 from src.utils.spark_session import spark
 
-df = spark.read.text(["data/raw/access_log_Jul95", "data/raw/access_log_Aug95"])
-pattern = r'^(\S+)\s+\S+\s+\S+\s+\[([^\]]+)\]\s+"(\S+)\s+(\S+)\s+\S+"\s+(\d{3})\s+(\d+|-)$'
+def parse_raw_log(): 
+    df = spark.read.text(["data/raw/access_log_Jul95", "data/raw/access_log_Aug95"])
+    pattern = r'^(\S+)\s+\S+\s+\S+\s+\[([^\]]+)\]\s+"(\S+)\s+(\S+)\s+\S+"\s+(\d{3})\s+(\d+|-)$'
 
-df = df.select(
-    regexp_extract("value", pattern, 1).alias("host"),
-    regexp_extract("value", pattern, 2).alias("timestamp"),
-    regexp_extract("value", pattern, 3).alias("method"),
-    regexp_extract("value", pattern, 4).alias("endpoint"),
-    regexp_extract("value", pattern, 5).alias("status"),
-    regexp_extract("value", pattern, 6).alias("bytes"),
-)
-print(df.filter(df.host == "").count())
+    df = df.select(
+        regexp_extract("value", pattern, 1).alias("host"),
+        regexp_extract("value", pattern, 2).alias("timestamp"),
+        regexp_extract("value", pattern, 3).alias("method"),
+        regexp_extract("value", pattern, 4).alias("endpoint"),
+        regexp_extract("value", pattern, 5).alias("status"),
+        regexp_extract("value", pattern, 6).alias("bytes"),
+    )
+    return df
 
-# df.show()
-# df.printSchema()
+if __name__ == "__main__":
+    df = parse_raw_log()
+    df.write.csv("output_dir")
+
